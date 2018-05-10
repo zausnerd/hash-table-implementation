4f46cd82fd78e6de19d7de3aa46680d09abe8a30
zausnerd <zausnerd@gmail.com>
2018-05-10 05:55:10 -0400

    fix formatting and variable naming

--- ht.py
+++ ht.py
@@ -10,69 +10,68 @@ class HashTable:
         self.size = 0
         self.capacity = capacity
         self.table = [None for i in range(0, self.capacity)]
-    
+
     def get_load_factor(self):
         return self.size / self.capacity
-    
+
     def get_idx(self, key):
         return hash(key) % self.capacity
-        
+
     def get(self, key):
-        ptr = self.table[self.get_idx(key)]
-        while ptr != None:
-            if ptr.key == key:
-                return ptr.val
-            ptr = ptr.next
+        entry = self.table[self.get_idx(key)]
+        while entry is not None:
+            if entry.key == key:
+                return entry.val
+            entry = entry.next
         raise KeyError(key)
 
     def set(self, key, val):
         idx = self.get_idx(key)
-        ptr = self.table[idx]
-        if ptr != None and ptr.key == key:
-            ptr.val = val
+        entry = self.table[idx]
+        if entry is not None and entry.key == key:
+            entry.val = val
             return
-        if self.table[idx] == None:
+        if self.table[idx] is None:
             self.table[idx] = self.Entry(key, val)
         else:
-            while ptr.next != None:
-                if ptr.key == key:
-                    ptr.val = val
+            while entry.next is not None:
+                if entry.key == key:
+                    entry.val = val
                     return
-                ptr = ptr.next
-            ptr.next = self.Entry(key, val)
-        self.size += 1    
+                entry = entry.next
+            entry.next = self.Entry(key, val)
+        self.size += 1
         if self.get_load_factor() >= 0.5:
             self.resize()
         return
 
     def delete(self, key):
         idx = self.get_idx(key)
-        if self.table[idx] == None:
+        if self.table[idx] is None:
             raise KeyError(key)
         else:
-            ptr = self.table[idx]
-            if ptr.key == key:
-                self.table[idx] = ptr.next
+            entry = self.table[idx]
+            if entry.key == key:
+                self.table[idx] = entry.next
                 self.size -= 1
                 return
-            while ptr != None:
-                next_ptr = ptr.next
-                if next_ptr != None and next_ptr.key == key:
-                    ptr.next = next_ptr.next
-                    self.size -= 1 
+            while entry is not None:
+                next_entry = entry.next
+                if next_entry is not None and next_entry.key == key:
+                    entry.next = next_entry.next
+                    self.size -= 1
                     return
-                ptr = ptr.next
-            raise KeyError(key)            
-    
+                entry = entry.next
+            raise KeyError(key)
+
     def resize(self):
         old_table = self.table
         self.table = [None for i in range(0, self.capacity * 2)]
         self.capacity = self.capacity * 2
         self.size = 0
         for element in old_table:
-            if element != None:
-                ptr = element
-                while ptr != None:
-                    self.set(ptr.key, ptr.val)
-                    ptr = ptr.next
-
+            if element is not None:
+                entry = element
+                while entry is not None:
+                    self.set(entry.key, entry.val)
+                    entry = entry.next
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import unittest
 from ht import HashTable
 
+
 class instantiationTestCase(unittest.TestCase):
     def setUp(self):
         self.ht = HashTable()
+
     def testSize(self):
         """ Hash table should have initial size of 0"""
         assert self.ht.size == 0
@@ -16,13 +18,14 @@ class instantiationTestCase(unittest.TestCase):
         """ Should override default capacity"""
         self.ht = HashTable(200)
         assert self.ht.capacity == 200
-    
+
+
 class insertionTestCase(unittest.TestCase):
     def setUp(self):
         self.ht = HashTable()
         self.ht.set('foo', 'bar')
         self.ht.set(('foo', 'bar'), 'bam')
-    
+
     def testInsertSingle(self):
         """ Insertion should increase size """
         assert self.ht.size == 2
@@ -36,10 +39,11 @@ class insertionTestCase(unittest.TestCase):
         """ Should be able to get what was inserted """
         assert self.ht.get('foo') == 'bar'
         assert self.ht.get(('foo', 'bar')) == 'bam'
-    
+
     def testErrorLookup(self):
         """ Error should be thrown if no key in table """
         self.assertRaises(KeyError, self.ht.get, 'blah')
+
 
 class collisionTestCase(unittest.TestCase):
     def setUp(self):
@@ -47,29 +51,30 @@ class collisionTestCase(unittest.TestCase):
         self.ht.set(14, 'fourteen')
         self.ht.set(114, 'oneFourteen')
         self.ht.set(214, 'twoFourteen')
-    
+
     def testHash(self):
         """ All three keys should hash to the same idx in table """
         assert self.ht.get_idx(14) == 14
         assert self.ht.get_idx(114) == 14
         assert self.ht.get_idx(214) == 14
-    
+
     def testSeperateChaining(self):
         """ Should be able to properly look up all keys, even with collision """
         assert self.ht.get(14) == 'fourteen'
         assert self.ht.get(114) == 'oneFourteen'
         assert self.ht.get(214) == 'twoFourteen'
 
+
 class resizeTestCase(unittest.TestCase):
     def setUp(self):
         self.ht = HashTable()
         for i in range(0, 40):
             self.ht.set(i, i)
-    
+
     def testLoadFactor(self):
         """ Should properly calculate the load factor, 0.4 (size / capacity) """
         assert self.ht.get_load_factor() == 0.4
-    
+
     def testResize(self):
         """ Capacity should double once the load factor is >= 0.5"""
         for i in range(40, 50):
@@ -81,8 +86,10 @@ class resizeTestCase(unittest.TestCase):
         """ After resizing, all elements should still be in table """
         for i in range(40, 50):
             self.ht.set(i, i)
-        for i in range(0,50):
+        for i in range(0, 50):
             assert self.ht.get(i) == i
+
+
 class deleteTestCase(unittest.TestCase):
     def setUp(self):
         self.ht = HashTable()
@@ -91,36 +98,40 @@ class deleteTestCase(unittest.TestCase):
         self.ht.set(214, 'twoFourteen')
         self.ht.set(314, 'threeFourteen')
         self.ht.set('foo', 'bar')
-    
+
     def testDeleteNonLinkedEntry(self):
         """ Delete a single entry that is not connected to other entries"""
         self.ht.delete('foo')
         self.assertRaises(KeyError, self.ht.get, 'foo')
-    
+
     def testDeleteFirstLinkedEntry(self):
-        """ Delete the first connected entry, but still have access to other connected entries"""
+        """ Delete the first connected entry, but still have
+        access to other connected entries"""
         self.ht.delete(14)
         self.assertRaises(KeyError, self.ht.get, 14)
         assert self.ht.get(114) == 'oneFourteen'
         assert self.ht.get(214) == 'twoFourteen'
 
     def testDeleteIntermediateLinkedEntry(self):
-        """ Delete an intermediate connected entry, but still have access to other connected entries"""
+        """ Delete the middle connected entry, but still have
+        access to other connected entries"""
         self.ht.delete(214)
         self.assertRaises(KeyError, self.ht.get, 214)
         assert self.ht.get(14) == 'fourteen'
         assert self.ht.get(114) == 'oneFourteen'
         assert self.ht.get(314) == 'threeFourteen'
-    
+
     def testDeleteFinalLinkedEntry(self):
-        """ Delete the last connected entry, but still have access to other connected entries"""
+        """ Delete the last connected entry, but still have
+        access to other connected entries"""
         self.ht.delete(314)
         self.assertRaises(KeyError, self.ht.get, 314)
         assert self.ht.get(14) == 'fourteen'
         assert self.ht.get(214) == 'twoFourteen'
         assert self.ht.get(114) == 'oneFourteen'
+
     def testDeleteNonExistantKey(self):
         self.assertRaises(KeyError, self.ht.delete, 'George Costanza')
 
 if __name__ == "__main__":
-    unittest.main() 
+    unittest.main()
